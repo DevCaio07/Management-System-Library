fe948c1df26c005346c9d17d428b3d6fac027c49
Caio Guimaraes <caiotech.devbr@gmail.com>
2026-09-21 10:17:36 -0300

    elaborando o usuario

--- src/model/usuario.py
+++ src/model/usuario.py
@@ -1,9 +1,10 @@
+from repository.usuario_repository import Usuario_dados
 class Usuario_dados: 
     def __init__(self, nome, data_nascimento, idade, cadastro_CPF):
         self.nome_do_usuario = nome
         self.data_nascimento = data_nascimento
         self.idade_do_usuario = idade 
-        self.cadastro_CPF = cadastro_CPF
+        self._cadastro_CPFcadastro_CPF = cadastro_CPF
 
 @property
 def nome_do_usuario(self):
@@ -20,7 +21,28 @@ def idade_do_usuario(self):
 
 
 
+#Funcões da classe
 
-def InserirDados(Usuario_dados): 
+def InserirDados(self): 
+    print("-----Cadastro de Usuário-----")
     nome_do_usuario = input("Insira seu nome: ")
-    
+    self.enviar_para_lista(nome_do_usuario)
+
+    data_nascimento = input("Insira sua data de nascimento:")
+    self.enviar_para_lista(data_nascimento)
+
+    cadastro_CPF = input("Insira seu CPF:")
+    self.enviar_para_lista(cadastro_CPF)
+
+    idade_do_usuario = input("Insira sua idade") 
+    self.enviar_para_lista(idade_do_usuario)
+
+    Dados_usuarios = {
+         "Nome": nome_do_usuario,
+         "Data de Nascimento": data_nascimento,
+         "CPF": cadastro_CPF,
+         "Idade": idade_do_usuario
+    }
+
+
+    return Dados_usuarios
---
+++ src/repository/usuario_repository.py
@@ -0,0 +1,11 @@
+from model.usuario import Usuario_dados 
+#Sintaxe para importar dados e atributos de uma outra classe
+
+
+class Guardar_dados: 
+    def __init__(self):
+       self.Lista_de_usuarios = []
+
+
+def salvar_usuario():
+    
